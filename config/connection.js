eba9c7651cfe13d24d1b3dc07a77480a8d20a30f
Lauren <l.cvengros@icloud.com>
2022-10-19 13:55:05 -0400

    added connection.js and schema.sql

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+//https://pacific-crag-42057.herokuapp.com/ 
+
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
